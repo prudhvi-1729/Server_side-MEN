6f97a97d4c02c565d6953cc6c4a43a8f7574a14f
Prudhvi kumar Baddam <prudhvibaddam@gmail.com>
2021-01-21 15:09:06 +0530

    passport jwt authentication

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ const dburl = "mongodb://127.0.0.1:27017/conFusion"
 const { compile } = require('morgan');
 const { Buffer } = require('buffer');
 const connect = mongoose.connect(dburl);
-
+var passport = require('passport');
+var authenticate = require('./routes/authenticate');
 //database connection
 connect.then((db)=>{
   console.log("connected correctly to database")
@@ -35,36 +36,23 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser('12345-67890-09876-54321'));
 
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()
-}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-function auth(req,res,next){
-  console.log(req.session);
-   //!req.signedCookie.user
-  if(!req.session.user){
+function auth (req, res, next) {
+  console.log(req.user);
+
+  if (!req.user) {
     var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
-  }
-  else{
-  if(req.session.user === 'authenticated'){
-    next();
-  }
-  else{
-    var err = new Error('You are not authenticated!');
-    err.status=401;
+    err.status = 403;
     next(err);
   }
-}
-  
+  else {
+        next();
+  }
 }
 
 app.use(auth);
